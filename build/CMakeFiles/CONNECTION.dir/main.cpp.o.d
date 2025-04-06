971137db2a242cb1e70ce1a2d5e7207b4a594dce
guanyi_mu <15810345904@163.com>
2025-04-06 19:36:48 +0800

    提交代码，实现多线程压力测试

--- build/CMakeFiles/CONNECTION.dir/main.cpp.o.d
+++ build/CMakeFiles/CONNECTION.dir/main.cpp.o.d
@@ -148,6 +148,29 @@ CMakeFiles/CONNECTION.dir/main.cpp.o: \
  /usr/include/c++/11/bits/istream.tcc /usr/include/c++/11/chrono \
  /usr/include/c++/11/ratio /usr/include/c++/11/limits \
  /usr/include/c++/11/ctime /usr/include/c++/11/bits/parse_numbers.h \
+ /usr/include/c++/11/queue /usr/include/c++/11/deque \
+ /usr/include/c++/11/bits/stl_uninitialized.h \
+ /usr/include/c++/11/bits/stl_deque.h /usr/include/c++/11/bits/deque.tcc \
+ /usr/include/c++/11/vector /usr/include/c++/11/bits/stl_vector.h \
+ /usr/include/c++/11/bits/stl_bvector.h \
+ /usr/include/c++/11/bits/vector.tcc /usr/include/c++/11/bits/stl_heap.h \
+ /usr/include/c++/11/bits/stl_queue.h \
+ /usr/include/c++/11/bits/uses_allocator.h /usr/include/c++/11/functional \
+ /usr/include/c++/11/tuple /usr/include/c++/11/utility \
+ /usr/include/c++/11/bits/stl_relops.h /usr/include/c++/11/array \
+ /usr/include/c++/11/bits/invoke.h /usr/include/c++/11/bits/refwrap.h \
+ /usr/include/c++/11/bits/std_function.h \
+ /usr/include/c++/11/unordered_map \
+ /usr/include/c++/11/ext/aligned_buffer.h \
+ /usr/include/c++/11/bits/hashtable.h \
+ /usr/include/c++/11/bits/hashtable_policy.h \
+ /usr/include/c++/11/bits/enable_special_members.h \
+ /usr/include/c++/11/bits/node_handle.h \
+ /usr/include/c++/11/bits/unordered_map.h \
+ /usr/include/c++/11/bits/erase_if.h /usr/include/c++/11/bits/stl_algo.h \
+ /usr/include/c++/11/bits/algorithmfwd.h \
+ /usr/include/c++/11/bits/stl_tempbuf.h \
+ /usr/include/c++/11/bits/uniform_int_dist.h \
  /home/xiangbei/project/MySQL_connection/include/connection.hpp \
  /usr/include/mysql/mysql.h \
  /usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h \
@@ -161,41 +184,21 @@ CMakeFiles/CONNECTION.dir/main.cpp.o: \
  /usr/include/mysql/errmsg.h \
  /home/xiangbei/project/MySQL_connection/include/connectionPool.hpp \
  /home/xiangbei/project/MySQL_connection/include/connection.hpp \
- /usr/include/c++/11/mutex /usr/include/c++/11/tuple \
- /usr/include/c++/11/utility /usr/include/c++/11/bits/stl_relops.h \
- /usr/include/c++/11/array /usr/include/c++/11/bits/uses_allocator.h \
- /usr/include/c++/11/bits/invoke.h /usr/include/c++/11/bits/std_mutex.h \
- /usr/include/c++/11/bits/unique_lock.h /usr/include/c++/11/queue \
- /usr/include/c++/11/deque /usr/include/c++/11/bits/stl_uninitialized.h \
- /usr/include/c++/11/bits/stl_deque.h /usr/include/c++/11/bits/deque.tcc \
- /usr/include/c++/11/vector /usr/include/c++/11/bits/stl_vector.h \
- /usr/include/c++/11/bits/stl_bvector.h \
- /usr/include/c++/11/bits/vector.tcc /usr/include/c++/11/bits/stl_heap.h \
- /usr/include/c++/11/bits/stl_queue.h /usr/include/c++/11/atomic \
+ /usr/include/c++/11/mutex /usr/include/c++/11/bits/std_mutex.h \
+ /usr/include/c++/11/bits/unique_lock.h /usr/include/c++/11/atomic \
  /usr/include/c++/11/bits/atomic_base.h \
  /usr/include/c++/11/bits/atomic_lockfree_defines.h \
  /usr/include/c++/11/thread /usr/include/c++/11/bits/std_thread.h \
- /usr/include/c++/11/bits/refwrap.h /usr/include/c++/11/bits/unique_ptr.h \
+ /usr/include/c++/11/bits/unique_ptr.h \
  /usr/include/c++/11/bits/this_thread_sleep.h /usr/include/c++/11/memory \
- /usr/include/c++/11/bits/stl_tempbuf.h \
  /usr/include/c++/11/bits/stl_raw_storage_iter.h \
  /usr/include/c++/11/bits/align.h /usr/include/c++/11/bit \
  /usr/include/c++/11/bits/shared_ptr.h \
  /usr/include/c++/11/bits/shared_ptr_base.h \
  /usr/include/c++/11/bits/allocated_ptr.h \
- /usr/include/c++/11/ext/aligned_buffer.h \
  /usr/include/c++/11/ext/concurrence.h \
  /usr/include/c++/11/bits/shared_ptr_atomic.h \
  /usr/include/c++/11/backward/auto_ptr.h \
  /usr/include/c++/11/pstl/glue_memory_defs.h \
- /usr/include/c++/11/pstl/execution_defs.h /usr/include/c++/11/functional \
- /usr/include/c++/11/bits/std_function.h \
- /usr/include/c++/11/unordered_map /usr/include/c++/11/bits/hashtable.h \
- /usr/include/c++/11/bits/hashtable_policy.h \
- /usr/include/c++/11/bits/enable_special_members.h \
- /usr/include/c++/11/bits/node_handle.h \
- /usr/include/c++/11/bits/unordered_map.h \
- /usr/include/c++/11/bits/erase_if.h /usr/include/c++/11/bits/stl_algo.h \
- /usr/include/c++/11/bits/algorithmfwd.h \
- /usr/include/c++/11/bits/uniform_int_dist.h \
+ /usr/include/c++/11/pstl/execution_defs.h \
  /usr/include/c++/11/condition_variable
--- main.cpp
+++ main.cpp
@@ -1,50 +1,36 @@
 #include <iostream>
 #include <time.h>
 #include <chrono>
+#include <queue>
+#include <functional>
 #include "connection.hpp"
 #include "connectionPool.hpp"
 using namespace std;
 
+int times;
+int thread_num = 10;
 char sql[1024] = "insert into users(name, age) values('zhang san', 20);";
 
-void test_without_pool(int times)
+void test_without_pool()
 {
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < times; ++i)
+    for (int i = 0; i < times / thread_num; ++i)
     {
         Connection conn;
         conn.connect("127.0.0.1", 3306, "root", "123456", "project_mysql_connection_pool");
         conn.update(sql);
     }
-
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double> duration = end - start;
-
-    std::cout << "不用连接池运行" << times << "次" << endl
-              << "耗时: " << duration.count() << " 秒" << endl
-              << endl;
 }
 
-void test_with_pool(int times)
+void test_with_pool()
 {
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < times; ++i)
+    for (int i = 0; i < times / thread_num; ++i)
     {
         ConnectionPool *cp = ConnectionPool::getConnectionPool();
         auto sp = cp->getConnection();
         sp->update(sql);
     }
-
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double> duration = end - start;
-
-    std::cout << "用连接池运行" << times << "次" << endl
-              << "耗时: " << duration.count() << " 秒" << endl
-              << endl;
 }
 
 void clear_mysql()
@@ -55,16 +41,49 @@ void clear_mysql()
     conn.update("create table users(id INT AUTO_INCREMENT PRIMARY KEY, name varchar(50) NOT NULL, age int NOT NULL);");
 }
 
+void run_test(bool use_pool)
+{
+
+    vector<thread> vec;
+
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < thread_num; ++i)
+    {
+        if (use_pool)
+            vec.emplace_back(test_with_pool);
+        else
+            vec.emplace_back(test_without_pool);
+    }
+
+    for (int i = 0; i < thread_num; ++i)
+    {
+        vec[i].join();
+    }
+
+    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration = end - start;
+
+    if (use_pool)
+        cout << "用连接池运行" << times << "次" << endl;
+    else
+        cout << "不用连接池运行" << times << "次" << endl;
+
+    cout << "耗时: " << duration.count() << " 秒" << endl
+         << endl;
+}
+
 int main()
 {
-    int times;
     while (true)
     {
         cin >> times;
         clear_mysql();
-        test_with_pool(times);
+        run_test(true);
+
         clear_mysql();
-        test_without_pool(times);
+        run_test(false);
     }
 
     return 0;
